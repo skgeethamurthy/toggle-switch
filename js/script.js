1b6d81cb4a8abf01d4666273f187c965e0d52be3
skgeethamurthy <skgeethamurthy@gmail.com>
2022-08-07 18:58:48 +0530

    script-added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-// console.log("test")
+var btn= document.getElementById("btn");
+var light= document.getElementById("light");
+
+function toggleBtn(){
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+}
